6ed2c2f70cbfa552fd4e9a576bbe04e491e6e451
jenberu <jemberu0970@gmail.com>
2024-05-22 21:36:06 +0300

    C:/Program Files/Git/home/index.jsx is updated

--- src/contianer/home/index.jsx
+++ src/contianer/home/index.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import "./style.scss";
 const HomePge = () => {
-    return (
-        <div>
-          Home  page
-        </div>
-    );
-}
+  return (
+    <section id="home" className="home">
+      <div className="home__text__wrapper">
+        <h1>
+          Hello, I'm Jemberu kassie
+          <br />
+          fullstack developer
+        </h1>
+      </div>
+    </section>
+  );
+};
 export default HomePge;
