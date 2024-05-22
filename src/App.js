55a09b933c7606489f8ceb3ca9a40ab6483d7eda
jenberu <jemberu0970@gmail.com>
2024-05-22 21:37:58 +0300

    App.js is updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./App.scss";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePge from "./contianer/home";
 import About from "./contianer/about";
@@ -6,19 +6,24 @@ import ResumePge from "./contianer/resume";
 import MySkillPge from "./contianer/skills";
 import PortifoloPage from "./contianer/portifolo";
 import ContactPage from "./contianer/contact";
-import Particles from "@tsparticles/react";
-import { Engine } from "@tsparticles/engine";
+import NavBar from "./components/navBar";
+
 function App() {
   return (
     <div className="App">
+      
+      <NavBar />
+      <div className="App__main-page-content">
       <Routes>
         <Route index path="/" element={<HomePge />} />
         <Route path="/about" element={<About />} />
         <Route path="/resume" element={<ResumePge />} />
         <Route path="/skill" element={<MySkillPge />} />
-        <Route path="/portifolo" element={<PortifoloPage />} />
+        <Route path="/portfolo" element={<PortifoloPage />} />
         <Route path="/contact" element={<ContactPage />} />
       </Routes>
+      </div>
+      
     </div>
   );
 }
