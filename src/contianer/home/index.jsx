0d865aec7d879ec53947dc5b814e563dcc97c192
jenberu <jemberu0970@gmail.com>
2024-05-22 23:08:59 +0300

    updated

--- src/contianer/home/index.jsx
+++ src/contianer/home/index.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import "./style.scss";
+import { useNavigate } from "react-router-dom";
+import { Animate } from "react-simple-animate";
 const HomePge = () => {
+  const navigate = useNavigate();
+  const handleNavigateTocontactMe = () => {
+    navigate("/contact");
+  };
   return (
     <section id="home" className="home">
       <div className="home__text__wrapper">
@@ -10,6 +16,21 @@ const HomePge = () => {
           fullstack developer
         </h1>
       </div>
+      <Animate
+        play
+        duration={1.5}
+        delay={1}
+        start={{
+          transform: "translateY(550px)",
+        }}
+        end={{
+          transform: "translateX(0px)",
+        }}
+      >
+        <div className="home__contact-me">
+          <button onClick={handleNavigateTocontactMe}>hire me</button>
+        </div>
+      </Animate>
     </section>
   );
 };
