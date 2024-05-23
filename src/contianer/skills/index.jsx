1b28ddab13b512338bb478da4f21dbbe458ddd1b
jenberu <jemberu0970@gmail.com>
2024-05-23 11:36:22 +0300

    updated

--- src/contianer/skills/index.jsx
+++ src/contianer/skills/index.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { BsInfoCircleFill} from 'react-icons/bs';
+import PageHeaderContent from "../../components/pageheaderContent";
 const MySkillPge = () => {
     return (
-        <div>
-            my skill page
-        </div>
+        <section id="skills" className="skills">
+        <PageHeaderContent
+          headerText="My Skills"
+          icon={<BsInfoCircleFill size={40} />}
+        />
+      </section>
     );
 }
 export default MySkillPge;
