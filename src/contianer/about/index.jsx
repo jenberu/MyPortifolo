e02d5122ae25f020e4eb116305b2a66c27a170fe
jenberu <jemberu0970@gmail.com>
2024-05-23 11:27:48 +0300

    updated

--- src/contianer/about/index.jsx
+++ src/contianer/about/index.jsx
@@ -1,13 +1,112 @@
 import React from "react";
 import { BsInfoCircleFill} from 'react-icons/bs';
 import PageHeaderContent from "../../components/pageheaderContent";
+import { Animate } from 'react-simple-animate';
+import { DiApple, DiAndroid } from 'react-icons/di';
+import { FaDev,FaDatabase } from 'react-icons/fa';
+
+import './style.scss';
+const personalDetial = [
+  {
+    label: 'Name',
+    value: 'Jemberu Kassie',
+  },
+  {
+    label: 'Age',
+    value: '24',
+  },
+  {
+    label: 'Address',
+    value: 'Bahir Dar',
+  },
+  {
+    label: 'Email',
+    value: 'jemberu0970@gmail.com',
+  },
+  {
+    label: 'Mobile No',
+    value: '0970013586',
+  },
+];
+const aboutMEsummery=' BSC in Software Engineering 4th year student in Poly Institute of technology, Bahir Dar University'
+
 const About = () => {
   return (
     <section id='about' className="about">
       <PageHeaderContent headerText='About me'
         icon={<BsInfoCircleFill size={40}/>}
       />
+      <div className="about__content">
+        <div className="about__content__personalWrapper">
+        <Animate
+        play
+        duration={1.5}
+        delay={1}
+        start={{
+          transform: "translateX(-900px)",
+        }
+        }
+        end={{
+          transform: "translatex(0px)",
+        }}
+      >
+        <h3>fullstack Developer</h3>
+          <p>{aboutMEsummery}</p>
+        </Animate>
+        <Animate
+        play
+        duration={1.5}
+        delay={1}
+        start={{
+          transform: "translateX(500px)",
+        }
+        }
+        end={{
+          transform: "translatex(0px)",
+        }}
+      >
+        <h3 className="personalInformationHeaderText">Personal Information </h3>
+        <ul>
+          {
+            personalDetial.map((item, i)=>(
+              <li key={i}>
+                <span className="title">
+                  {item.label}
+                </span>
+                <span className="value">
+                  {item.value}
+              </span>
+            </li>
+            ))
+          }
+          </ul>
+          </Animate>
+        </div>
+        <div className="about__content__serviceWrapper">
+        <Animate
+        play
+        duration={1.5}
+        delay={1}
+        start={{
+          transform: "translateX(600px)",
+        }
+        }
+        end={{
+          transform: "translatex(0px)",
+        }}
+      >
+          <div className="about__content__serviceWrapper__innerContent">
+          <div><FaDev size={60} color="blue" />
+          </div>
+          <div><DiAndroid size={60} color="blue" /></div>
+          <div><FaDatabase size={60} color="blue" /></div>
+          <div><DiApple size={60} color="blue" /></div>
+          </div>
+        </Animate>
+        </div>
 
+      
+</div>
     
     </section>
   );
