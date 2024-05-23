c3001fbee6eee75f8af90d1a16722bffe190d800
jenberu <jemberu0970@gmail.com>
2024-05-23 11:32:43 +0300

    portifolo/index.jsx updated

--- src/contianer/portifolo/index.jsx
+++ src/contianer/portifolo/index.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { BsInfoCircleFill} from 'react-icons/bs';
+import PageHeaderContent from "../../components/pageheaderContent";
 const PortifoloPage = () => {
     return (
-        <div>
-            my portifolo page
-        </div>
+        <section id="portfolo" className="portfolo">
+      <PageHeaderContent
+        headerText="My Portfolo"
+        icon={<BsInfoCircleFill size={40} />}
+      />
+    </section>
     );
 }
 export default PortifoloPage;
