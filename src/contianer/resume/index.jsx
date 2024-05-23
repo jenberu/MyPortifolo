e764bde3fa518801e08806e8df17fb16cb20969f
jenberu <jemberu0970@gmail.com>
2024-05-23 11:35:54 +0300

    resume/index.jsx updated

--- src/contianer/resume/index.jsx
+++ src/contianer/resume/index.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { BsInfoCircleFill} from 'react-icons/bs';
+import PageHeaderContent from "../../components/pageheaderContent";
 const ResumePge = () => {
     return (
-        <div>
-          my resume page
-        </div>
+      <section id="resume" className="resume">
+      <PageHeaderContent
+        headerText="My Resume"
+        icon={<BsInfoCircleFill size={40} />}
+      />
+    </section>
     );
 }
 export default ResumePge;
