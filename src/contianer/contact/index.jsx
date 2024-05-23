cb70c8187e0e7d25cec43c779f2de6a351ab989b
jenberu <jemberu0970@gmail.com>
2024-05-23 11:29:48 +0300

    updated

--- src/contianer/contact/index.jsx
+++ src/contianer/contact/index.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { BsInfoCircleFill} from 'react-icons/bs';
+import PageHeaderContent from "../../components/pageheaderContent";
 const ContactPage = () => {
-    return (
-        <div>
-         my contact page
-        </div>
-    );
-}
+  return (
+    <section id="contact" className="contact">
+      <PageHeaderContent
+        headerText="Contact Me"
+        icon={<BsInfoCircleFill size={40} />}
+      />
+    </section>
+  );
+};
 export default ContactPage;
