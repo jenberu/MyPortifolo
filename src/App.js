9d29f8b1954712d2a6c0b326dc8372afe4d9bfb0
jenberu <jemberu0970@gmail.com>
2024-05-29 11:41:52 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.scss";
-import { BrowserRouter as Route, Routes } from "react-router-dom";
+import {  Route, Routes } from "react-router-dom";
 import HomePge from "./contianer/home";
 import About from "./contianer/about";
 import ResumePge from "./contianer/resume";
